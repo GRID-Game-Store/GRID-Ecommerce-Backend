7e5ce56958ef277852183ce717275606ec3c093d
SEM <samerx777222@gmail.com>
2023-11-11 21:52:19 +0100

    Configured the Cucumber and TestContainer for tests.

---
+++ src/test/java/com/khomsi/grid/game/GameServiceTest.java
@@ -0,0 +1,9 @@
+package com.khomsi.grid.game;
+
+import com.khomsi.grid.game.config.SpringIntegration;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class GameServiceTest extends SpringIntegration {
+
+}
---
+++ src/test/java/com/khomsi/grid/game/config/SpringIntegration.java
@@ -0,0 +1,7 @@
+package com.khomsi.grid.game.config;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+
+@CucumberContextConfiguration
+public class SpringIntegration extends BaseIntegration {
+}
