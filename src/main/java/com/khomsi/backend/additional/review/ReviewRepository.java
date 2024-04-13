213a80f46ea7ffda999c6c716638abdaec2d46c3
SEM <samerx777222@gmail.com>
2024-04-13 16:32:33 +0200

    Code refactor, added new endpoint.

--- src/main/java/com/khomsi/backend/additional/review/ReviewRepository.java
+++ src/main/java/com/khomsi/backend/additional/review/ReviewRepository.java
@@ -1,6 +1,8 @@
 package com.khomsi.backend.additional.review;
 
 import com.khomsi.backend.additional.review.model.entity.Review;
+import com.khomsi.backend.main.game.model.entity.Game;
+import com.khomsi.backend.main.user.model.entity.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,4 +11,5 @@ import java.util.List;
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     @Query("SELECT r FROM Review r WHERE r.games.id = :gameId order by r.reviewDate DESC")
     List<Review> findAllByGameIdOrderByReviewDate(Long gameId);
+    Review findByUsersAndGames(UserInfo user, Game game);
 }
