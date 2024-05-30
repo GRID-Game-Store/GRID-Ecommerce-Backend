addc40724899128d3bd412f611157973e2e2fd2f
SEM <samerx777222@gmail.com>
2024-05-30 17:45:19 +0200

    Release date fix in GameRequest

--- src/main/java/com/khomsi/backend/main/game/model/entity/Game.java
+++ src/main/java/com/khomsi/backend/main/game/model/entity/Game.java
@@ -47,6 +47,7 @@ public class Game {
     @Lob
     @Column(name = "description")
     private String description;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     @Column(name = "release_date")
     private LocalDate releaseDate;
     @NotNull
